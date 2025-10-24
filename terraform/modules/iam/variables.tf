3c741a4eb80ed0807d000dd170cd487760b1d07e
KhaledSaiidi <khaled.saiidi@outlook.com>
2025-10-24 21:11:30 +0100

    iam module

---
+++ terraform/modules/iam/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  description = "Project or environment prefix used for IAM resource names."
+  type        = string
+}
+
+variable "tags" {
+  description = "Additional tags to apply to all IAM resources."
+  type        = map(string)
+  default     = {}
+}
