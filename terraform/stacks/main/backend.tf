583b23236a013734923575f6d57a9394abfc4af1
KhaledSaiidi <khaled.saiidi@outlook.com>
2025-10-24 20:33:04 +0100

    prepare backend & stack

---
+++ terraform/stacks/main/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "devops-autopilot-bucket"
+    key    = "devops-autopilot.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "devops-autopilot-dynamo-table"
+  }
+}
