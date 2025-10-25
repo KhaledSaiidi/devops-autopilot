4043ee15ec1eeb6b76c36dd092b10e189d7c90c6
KhaledSaiidi <khaled.saiidi@outlook.com>
2025-10-25 19:12:56 +0100

    fix: kms authorizes EKS control-plane role

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -3,6 +3,9 @@ output "eks_cluster_role_arn" {
   value       = aws_iam_role.eks_cluster_role.arn
 }
 
+output "eks_cluster_role_name" { 
+  value = aws_iam_role.eks_cluster_role.name 
+}
 output "eks_node_role_arn" {
   description = "IAM role ARN for the EKS worker nodes."
   value       = aws_iam_role.eks_node_role.arn
