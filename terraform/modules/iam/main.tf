559ec381b5f5a4fb2ffd4c7e7c7c8e2a6a7bc4c1
KhaledSaiidi <khaled.saiidi@outlook.com>
2026-03-17 00:03:41 +0100

    test: test GitHub Actions workflow

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -21,9 +21,6 @@ resource "aws_iam_role" "eks_cluster_role" {
     var.tags
   )
 }
-
-
-
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
